064612597b3e08631467b617affeb3342fec347c
jbdumkl <gglmkl@gmail.com>
2024-01-15 17:18:32 +0900

    myProfileProject-240115

---
+++ src/main/java/com/mkl/home/MyProfileProject20240115Application.java
@@ -0,0 +1,13 @@
+package com.mkl.home;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyProfileProject20240115Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyProfileProject20240115Application.class, args);
+	}
+
+}
---
+++ src/main/java/com/mkl/home/dao/BoardDao.java
@@ -0,0 +1,6 @@
+package com.mkl.home.dao;
+
+public interface BoardDao {
+	public String testDao(String fbnum);//글 번호 입력 시 해당글의 제목 반환
+	
+}
